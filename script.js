fe7272be9da8dd894c1a1918d49a1975ac0a0ee6
M.Ehsan <124778596+ehsan-saf@users.noreply.github.com>
2023-09-14 11:32:57 +0330

    Print gameBoard arrau in cells

--- script.js
+++ script.js
@@ -12,18 +12,38 @@ const gameController = (() => {
 
 const gameBoard = (() =>{
 
-    const arr = [];
+    const arr = ["x", "x", "x", "o", "o","x", "x", "x","o"];
+    
 
-    return {array: arr};
+    function add(marker) {
+        arr.push(marker);
+    }
+
+    return {array: arr, add};
 
 })();
 
 const displayController = (() => {
 
+    const cross = "x";
+    const circle = "o";
+    const cells = document.querySelectorAll(".cell");
+
     function fillCells() {
-        
+        for(let i = 0; i < 9; i++) {
+            cells[i].textContent = gameBoard.array[i];
+        }
     }
+
+    function clearCells() {
+
+    }
+
+    return {fillCells, clearCells};
 
 })();
 
-gameController.resetGame();
+const player = (name, marker) => {
+    return {name, marker};
+};
+
